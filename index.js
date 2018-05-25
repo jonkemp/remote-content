e2bdd8e5a13118dcbd5fecde47729d684f2e28db
Mark Falk <mfalk@dotloop.com>
2018-05-25 15:11:25 -0700

    Added proxy support

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 'use strict';
 
 var superagent = require('superagent');
+var getProxyForUrl = require('proxy-from-env').getProxyForUrl;
+
+// extend with Request#proxy()
+require('superagent-proxy')(superagent);
 
 module.exports = function (remoteUrl, callback) {
-    superagent.get(remoteUrl).buffer().end(function (err, resp) {
-        if (err) {
-            return callback(err);
-        } else if (resp.ok) {
-            return callback(null, resp.text);
-        }
-        callback(new Error('GET ' + remoteUrl + ' ' + resp.status));
-    });
+    var proxy_url = getProxyForUrl(remoteUrl);
+
+    if(proxy_url) {
+        superagent.get(remoteUrl).proxy(proxy_url).buffer().end(function (err, resp) {
+            if (err) {
+                return callback(err);
+            } else if (resp.ok) {
+                return callback(null, resp.text);
+            }
+            callback(new Error('GET ' + remoteUrl + ' ' + resp.status));
+        });
+    }
+    else {
+        superagent.get(remoteUrl).buffer().end(function (err, resp) {
+            if (err) {
+                return callback(err);
+            } else if (resp.ok) {
+                return callback(null, resp.text);
+            }
+            callback(new Error('GET ' + remoteUrl + ' ' + resp.status));
+        });
+    }
 };
