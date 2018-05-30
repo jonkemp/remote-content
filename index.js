d7ca30278943a21c2c7445f0d391f0f9260260fb
Mark Falk <mfalk@dotloop.com>
2018-05-30 15:10:23 -0700

    Feedback implemented

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
 'use strict';
 
-var superagent = require('superagent');
-var getProxyForUrl = require('proxy-from-env').getProxyForUrl;
+var superagent = require('superagent'),
+    getProxyForUrl = require('proxy-from-env').getProxyForUrl;
 
 // extend with Request#proxy()
 require('superagent-proxy')(superagent);
 
 module.exports = function (remoteUrl, callback) {
-    var proxy_url = getProxyForUrl(remoteUrl);
+    var proxyUrl = getProxyForUrl(remoteUrl),
+        superagentCallback = function (err, resp) {
+            if (err) {
+                return callback(err);
+            } else if (resp.ok) {
+                return callback(null, resp.text);
+            }
+            return callback(new Error('GET ' + remoteUrl + ' ' + resp.status));
+        };
 
-    if(proxy_url) {
-        superagent.get(remoteUrl).proxy(proxy_url).buffer().end(function (err, resp) {
-            if (err) {
-                return callback(err);
-            } else if (resp.ok) {
-                return callback(null, resp.text);
-            }
-            callback(new Error('GET ' + remoteUrl + ' ' + resp.status));
-        });
-    }
-    else {
-        superagent.get(remoteUrl).buffer().end(function (err, resp) {
-            if (err) {
-                return callback(err);
-            } else if (resp.ok) {
-                return callback(null, resp.text);
-            }
-            callback(new Error('GET ' + remoteUrl + ' ' + resp.status));
-        });
+    if (proxyUrl) {
+        superagent.get(remoteUrl).proxy(proxyUrl).buffer().end(superagentCallback);
+    } else {
+        superagent.get(remoteUrl).buffer().end(superagentCallback);
     }
 };
